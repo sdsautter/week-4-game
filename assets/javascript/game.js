b0ad193e2bd4090b2e1eef3c91a220f3a7c700f9
sdsautter <sdsautter@gmail.com>
2017-03-14 23:42:21 -0400

    Added styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,80 +6,82 @@ var gameOver = false;
 var wins = 0;
 var losses = 0;
 
-function newGame () {
-	gameOver = false;
-	userNumber = 0;
-	linkValues = [];
-	gameNumber = Math.floor(Math.random() * (120 - 19) + 19);
-	
-	$("#userNumber").html(userNumber);
-	$("#gameNumber").html(gameNumber);
-	$("#wins").html("Wins: " + wins);
-	$("#losses").html("Losses: " + losses);
-	
+function newGame() {
+    gameOver = false;
+    userNumber = 0;
+    linkValues = [];
+    gameNumber = Math.floor(Math.random() * (120 - 19) + 19);
 
-	while (linkValues.length < 4) {
-		var values = Math.floor((Math.random() * 12) + 1);
-		if (linkValues.indexOf(values) < 0) {
-		linkValues.push(values);
-		}
-	}  
+    $("#userNumber").html(userNumber);
+    $("#userNumber").removeClass("winning-number");
+    $("#userNumber").removeClass("losing-number");
+    $("#gameNumber").html(gameNumber);
+    $("#wins").html("Wins: " + wins);
+    $("#losses").html("Losses: " + losses);
+
+
+    while (linkValues.length < 4) {
+        var values = Math.floor((Math.random() * 12) + 1);
+        if (linkValues.indexOf(values) < 0) {
+            linkValues.push(values);
+        }
+    }
 
 }
 
-$("#nesLink").click(function() { 
-		if (gameOver === false) {
-		userNumber = userNumber + linkValues[0];
-		console.log(userNumber);
-		$("#userNumber").html(userNumber);
-		gameChecker();
-	}
-	});
-$("#snesLink").click(function() { 
-		if (gameOver === false) {
+$("#nesLink").click(function() {
+    if (gameOver === false) {
+        userNumber = userNumber + linkValues[0];
+        console.log(userNumber);
+        $("#userNumber").html(userNumber);
+        gameChecker();
+    }
+});
+$("#snesLink").click(function() {
+    if (gameOver === false) {
 
-		userNumber = userNumber + linkValues[1];
-		console.log(userNumber);
-		$("#userNumber").html(userNumber);
-		gameChecker();
-}
-	});
-$("#ootLink").click(function() { 
-		if (gameOver === false) {
+        userNumber = userNumber + linkValues[1];
+        console.log(userNumber);
+        $("#userNumber").html(userNumber);
+        gameChecker();
+    }
+});
+$("#ootLink").click(function() {
+    if (gameOver === false) {
 
-		userNumber = userNumber + linkValues[2];
-		console.log(userNumber);
-		$("#userNumber").html(userNumber);
-		gameChecker();
-}
-	});
-$("#botwLink").click(function() { 
-		if (gameOver === false) {
+        userNumber = userNumber + linkValues[2];
+        console.log(userNumber);
+        $("#userNumber").html(userNumber);
+        gameChecker();
+    }
+});
+$("#botwLink").click(function() {
+    if (gameOver === false) {
 
-		userNumber = userNumber + linkValues[3];
-		console.log(userNumber);
-		$("#userNumber").html(userNumber);
-		gameChecker();
-}
-	});
+        userNumber = userNumber + linkValues[3];
+        console.log(userNumber);
+        $("#userNumber").html(userNumber);
+        gameChecker();
+    }
+});
 
 
 function gameChecker() {
-		if (userNumber === gameNumber) {
-		gameOver = true;
-		wins++;
-		console.log(linkValues);
-		setTimeout(newGame, 3000);
-		console.log(linkValues);
-	} else if (userNumber > gameNumber) {
-		gameOver = true;
-		losses++;
-		console.log(linkValues);
-		setTimeout(newGame, 3000);
-		console.log(linkValues);
-	}
+    if (userNumber === gameNumber) {
+        gameOver = true;
+        wins++;
+        $("#userNumber").attr("class", "winning-number");
+        console.log(linkValues);
+        setTimeout(newGame, 3000);
+        console.log(linkValues);
+    } else if (userNumber > gameNumber) {
+        gameOver = true;
+        losses++;
+        $("#userNumber").attr("class", "losing-number");
+        console.log(linkValues);
+        setTimeout(newGame, 3000);
+        console.log(linkValues);
+    }
 }
 
 newGame();
-
-
